43ba7838d752902537a0bff56525f4fa23926b99
brohithkr <brohithkr@gmail.com>
2023-07-31 19:54:42 +0530

    partial changes

--- encryptor-validator/main.py
+++ encryptor-validator/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, Request, Header, Response
 from pydantic import BaseModel
-from typing import Annotated, Union
+from typing import Annotated, Union, List
 import pyqrcode
 from PassUtil import genPass
 import dbconnector as db
 from crypto import hashhex, signData, unsignData
 from configparser import ConfigParser
 import os
-from datetime import datetime
+from datetime import datetime,date,timedelta
 from pytz import timezone
 
 # config params
@@ -28,6 +28,7 @@ else:
     if None in (SERVERURL, HKEY):
         raise Exception("Please provide environment variables or .config.ini")
 
+DAILY_PASS_VALIDITY = timedelta(years=1)
 
 # Models
 
@@ -37,7 +38,6 @@ class User(BaseModel):
     name: Union[str, None] = None
     password: str
     section: Union[str, None] = None
-
 
 
 class StatusResponse(BaseModel):
@@ -61,12 +61,28 @@ class Pass(BaseModel):
     passType: str
     issueDate: str
 
-class reqVerification(BaseModel):
+
+class reqVer(BaseModel):
     uid: str
     pwd: str
-    signed_data: str
+    data: str
+
 
 # functions
+
+def is_valid_user(user_type,user: User):
+    validtypes = ["mentors", "verifiers"]
+    if user_type not in validtypes:
+        return StatusResponse(success=False, msg="Invalid type access")
+    conn = db.connect()
+    userdata = db.get_data(conn, user_type, user.uid)
+
+    if not userdata:
+        return StatusResponse(success=False, msg="invalid uid")
+
+    if userdata["password"] != hashhex(user.password):
+        return StatusResponse(success=False, msg="invalid password")
+    return StatusResponse(success=True, msg=f"Valid {user_type[:-1]}, Login successful")
 
 
 app = FastAPI()
@@ -114,7 +130,7 @@ async def is_valid_user(usertype: str, user: User) -> Union[StatusResponse, Pass
 
 
 @app.post("/api/issuepass")
-def issuepass(reqpass: reqPass, fullimage: bool = False) -> Pass:
+def issuepass(reqpass: reqPass, resp: Response, fullimage: bool = False) -> Union[Pass, StatusResponse]:
     conn = db.connect()
     mentor_data = db.get_data(conn, "mentors", reqpass.uid)
     student_data = db.get_data(conn, "students", reqpass.rno)
@@ -130,18 +146,22 @@ def issuepass(reqpass: reqPass, fullimage: bool = False) -> Pass:
 
     pass_data = db.get_data(conn, "passes", reqpass.rno)
 
+    alreadyOwns = False
+
     if pass_data:
         if reqpass.passType != pass_data["passType"]:
             return StatusResponse(
                 success=False,
                 msg=f"{student_data[reqpass.rno]} already owns a {passtype} pass.",
             )
+        alreadyOwns = True
         if not fullimage:
-            return Pass(rno=reqpass.rno, **pass_data)
+            return Pass(rno=reqpass.rno, **pass_data).dict().update("alreadyOwns",alreadyOwns)
         else:
             pass_data["rno"] = reqpass.rno
             pass_data["b64qr"] = genPass(pass_data, reqpass.passType).decode()
-            return Pass(**pass_data)
+            
+            return Pass(rno=reqpass.rno, **pass_data).dict().update("alreadyOwns",alreadyOwns)
 
     signedrno = signData(reqpass.rno, mentor_data["private_key"], reqpass.uid)
     signed_data = f"{signedrno}@{reqpass.uid}"
@@ -162,30 +182,74 @@ def issuepass(reqpass: reqPass, fullimage: bool = False) -> Pass:
     if fullimage:
         resPass.b64qr = genPass(student_data, reqPass.passType).decode()
 
-    return resPass
+    return resPass.dict().update("alreadyOwns",alreadyOwns)
 
-@app.post("/api/verify")
-def verifyPass(reqVer: reqVerification) -> Union[StatusResponse]:
-    verifier_data = db.get_data(conn, "verifiers", reqVer.uid)
-    if not verifier_data:
-        return StatusResponse(success=False, msg="Invalid verifier UID")
-    elif verifier_data["password"] != hashhex(reqVer.pwd):
-        return StatusResponse(success=False, msg="Invalid verifier password")
-    sign, uid = signed_data.split("@")
-    conn = db.connect()
-    mentor_key = db.get_data(conn, "mentors", uid)["public_key"]
-    rno = None
-    try:
-        rno = unsignData(sign, mentor_key)
-    except:
-        return StatusResponse(success=False, msg="Invalid Signature")
 
-    pass_data = db.get_data(conn, "passes", rno)
 
-    # if not pass_data:
-    #     return StatusResponse(success=False, msg="Invalid Pass")
-    
-    # student_data = db.get_data(conn, "students", rno)
+@app.post("/api/get_scan_history")
+def get_scan_history(req: reqVer):
+
+
+
+
+
+# @app.post("/api/get_history")
+# def get_history(reqVer: reqVerification) -> Union[List,StatusResponse]:
+#     verifier_data = db.get_data(conn, "verifiers", reqVer.uid)
+#     if not verifier_data:
+#         return StatusResponse(success=False, msg="Invalid verifier UID")
+#     elif verifier_data["password"] != hashhex(reqVer.pwd):
+#         return StatusResponse(success=False, msg="Invalid verifier password")
+#     sign, uid = signed_data.split("@")
+#     conn = db.connect()
+#     mentor_key = db.get_data(conn, "mentors", uid)["public_key"]
+#     rno = None
+#     try:
+#         rno = unsignData(sign, mentor_key)
+#     except:
+#         return StatusResponse(success=False, msg="Invalid Signature")
+
+#     pass_details = db.get_data(conn, "passes", rno)
+#     history = db.get_data(conn, "scan_history", rno)
+
+#     history_today = None
+
+#     if not pass_details:
+#         return StatusResponse(success=False, msg="Not Active Pass")
+
+#     if pass_details["passType"] == "daily":
+#         pass_issue_date = datetime.strptime(
+#             pass_details["issueDate"], "%d-%m-%Y"
+#         ).date()
+#         if date.today() - datetime.strptime(
+#             pass_details["issueDate"], "%d-%m-%Y"
+#         ).date() > timedelta(days=1):
+#             return StatusResponse(success=False, msg="Pass Expired")
+#         history_today = []
+#         for i in history[-2:]:
+#             idate = datetime.strptime(i, "%d-%m-%Y %H:%M:%S")
+#             today = datetime.now(timezone("Asia/Kolkata"))
+#             if (today - idate).days == 0:
+#                 history_today.append(i)
+#         # if len(history_today) >= 2:
+#         #     return history_today
+#     if pass_details["passType"] == "single-use":
+#         if len(history) >= 2:
+#             set_data(conn, "expired_passes", rno, pass_details)
+#             return history
+
+#     # if 
+
+#     db.set_data(
+#         conn,
+#         "scan_history",
+#         rno,
+#         [datetime.now(timezone("Asia/Kolkata")).strftime("%d-%m-%Y %H:%M:%S")],
+#     )
+
+
+#     return history
+
     # if not student_data:
     #     return StatusResponse(success=False, msg="Insuffitient data")
     # mentor_data = db.get_data(conn, "mentors", uid)
@@ -193,6 +257,9 @@ def verifyPass(reqVer: reqVerification) -> Union[StatusResponse]:
     # if not unsignData(rno, signed_data, mentor_data["public_key"]):
     #     return StatusResponse(success=False, msg="Invalid Pass")
     # return StatusResponse(success=True, msg="Pass Verified")
+
+
+
 
 @app.head("/wake_up")
 def wake_up():
